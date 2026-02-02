d8c1890e4fec2801f7d6d301b335896b1916fe3f
Ananth-NK <nkananthckh005@gmail.com>
2026-02-03 01:07:55 +0530

    Show only hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import styles from "./style";
 
-import {
-  Navbar,
-  Hero,
-  Education,
-  SkillsAndExperience,
-  ExtraCurricular,
-  Footer,
-  OpenSource,
-  Projects,
-  BlogPosts,
-  Loading,
-  Achievements,
-} from "./components";
+import { Hero, Loading } from "./components";
 
 const App = () => {
   const [isLoading, setIsLoading] = React.useState(true);
@@ -26,7 +14,6 @@ const App = () => {
   }, []);
 
   return (
-    // A div to wrap the entire application
     <div className="bg-primary w-full overflow-hidden">
       <AnimatePresence>
         {isLoading ? (
@@ -38,38 +25,11 @@ const App = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, ease: "easeOut" }}
           >
-            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-              <div className={`${styles.boxWidth}`}>
-                <Navbar />
-              </div>
-            </div>
-
             <div className={`bg-primary ${styles.flexStart} pt-[80px]`}>
               <div className={`${styles.boxWidth}`}>
                 <Hero />
               </div>
             </div>
-
-            <div
-              className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}
-            >
-              <div className={`${styles.boxWidth}`}>
-                <SkillsAndExperience />
-                <Education />
-              </div>
-            </div>
-            <Achievements />
-            <div
-              className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}
-            >
-              <div className={`${styles.boxWidth}`}>
-                <Projects />
-                <BlogPosts enabled={false} />
-                <OpenSource />
-                <ExtraCurricular />
-              </div>
-            </div>
-            <Footer />
           </motion.section>
         )}
       </AnimatePresence>
